d043f56fb43b5f1494744522b1fa6c9171a0c36d
limon_148 <mehadihasan.cseru@gmail.com>
2017-09-22 22:15:54 -0700

    this is a initial code of this project

---
+++ src/com/ru/Main/ExportData.java
@@ -0,0 +1,17 @@
+package com.ru.Main;
+import edu.duke.*;
+import org.apache.commons.csv.*;
+
+public class ExportData {
+
+	public static void main(String[] args) {
+		ExportData ob = new ExportData();
+		ob.tester();
+	}
+
+	private void tester() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
