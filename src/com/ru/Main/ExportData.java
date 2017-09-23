0e50b226cd03603b46f683ba5feb8c78bb6c17e3
limon148 <mehadihasan.cseru@gmail.com>
2017-09-22 23:39:04 -0700

    this method listExportsTwoProducts prints the names of all the countries that export two iteams at a time

--- src/com/ru/Main/ExportData.java
+++ src/com/ru/Main/ExportData.java
@@ -14,6 +14,17 @@ public class ExportData {
 		CSVParser parser = fr.getCSVParser();
 		String info = countryinfo(parser, "Germany");
 		System.out.println(info);
+		parser = fr.getCSVParser();
+		listExportersTwoProducts(parser, "gold", "diamonds");
+	}
+
+	private void listExportersTwoProducts(CSVParser parser, String string, String string2) {
+		for(CSVRecord record : parser){
+			if(record.get("Exports").contains(string) && record.get("Exports").contains(string2)){
+				System.out.println(record.get("Country"));
+			}
+		}
+		
 	}
 
 	private String countryinfo(CSVParser parser, String string) {
